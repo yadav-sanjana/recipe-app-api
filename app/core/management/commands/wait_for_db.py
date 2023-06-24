5dc4dc51d0ebadde4a28068b4b044fd9eb429709
yadav-sanjana <sanjanay751@gmail.com>
2023-06-24 21:53:25 +0530

    configured docker-composer and git checks

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,6 @@
 import time
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg20pError
 from django.db.utils import OperationalError
 
 from django.core.management.base import BaseCommand
@@ -12,10 +12,9 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(database=['default'])
-                db_up: True
-            except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database currently unavailable, waiting 1 second...')
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database Available!'))

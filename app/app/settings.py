371ab871a6ee36db9d503c8bf7e0a7fdbf3e3b26
yadav-sanjana <sanjanay751@gmail.com>
2023-06-25 08:50:30 +0530

    create user, create superuser along with respective testcases

--- app/app/settings.py
+++ app/app/settings.py
@@ -60,10 +60,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST' : os.environ.get('DB_HOST'),
-        'NAME' : os.environ.get('DB_NAME'),
-        'USER' : os.environ.get('DB_USER'),
-        'PASSWORD' : os.environ.get('DB_PASS')
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
@@ -109,3 +109,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,50 @@
+"""
+Models testing
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """testing model create for user"""
+    def test_create_user_with_email_success(self):
+        """ testing user create successfully"""
+        email = "sanjana@example.com"
+        password = "Qwerty12"
+        user = get_user_model().objects.create_user(   # type: ignore
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """normalizing email input"""
+        sample_emails = [
+            ['test1@Example.com', 'test1@example.com'],
+            ['Test2@EXAMPLE.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+            ['test5@Example.Com', 'test5@example.com'],
+        ]
+
+        for emails, expected in sample_emails:
+            user = get_user_model().objects.create_user(  # type: ignore
+                emails, 'sample123') 
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_email_req_validation(self):
+        """should raise valueerror when email not passed"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(  # type: ignore
+                '', 'sample123')
+            
+    def test_create_superuser(self):
+        "create superuser"
+        user = get_user_model().objects.create_superuser(  # type: ignore
+            'test@exampler', 'sample123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
